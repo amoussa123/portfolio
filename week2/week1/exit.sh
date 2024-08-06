ccccdb5bd7a8dcb8d1571f5b965b8f4c3c8f8c8e
amoussa123 <moussa333@gmail.com>
2024-08-06 19:13:15 +1000

    Delete week2/week1 directory

--- week2/week1/exit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-clear
-echo "Hi there!"
-echo 'To find the exit code number for this script, type: echo $?'
-exit 22
-					
--- week2/week1/name2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-clear
-echo "Hi There!"
-echo 'PLease re-execute this file by typing: ./second.sh followed by your name.' 
-cowsay "It's good to see you $1!"
-exit 0
