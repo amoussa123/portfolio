77702edc11d9780f3fb8747142b5e3ac82c3587b
amoussa123 <moussa333@gmail.com>
2024-08-06 19:14:11 +1000

    Delete week2 directory

--- week2/if.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-clear
-read -p "Enter Value:    " x
-if [[ "$x" -ge 3 ]] && [[ "$x" -le 7 ]]; then
- echo "correct your number fell between the secret place (3-7)"
-else
- echo "wrong your number did not fall between the secret place (3-7)"
-fi
--- week2/names.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-clear
-echo "Type your first name and surname"
-read x y
-echo "Your first name  $x is cool but your surname $y sucks"
-exit 0
